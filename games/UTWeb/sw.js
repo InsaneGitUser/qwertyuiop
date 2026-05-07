2ffb1373873f43e511b60812d471eae960fb2ed1
InsaneGitUser <epikduck@duck.com>
2026-05-06 18:15:23 -0600

    Delete games directory

--- games/UTWeb/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  if (event.request.url.includes('game.unx')) {
-    event.respondWith(
-      fetch('http://77network.org/storage/game.unx')
-    );
-  }
-});
